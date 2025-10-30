4c5075b1a6c2904f7839979daf71f49178008f24
tcharles <titouancharles@gmail.com>
2025-10-30 17:06:04 +0100

    api route menu drinks et post orders

---
+++ main.go
@@ -0,0 +1,152 @@
+package main
+
+import (
+	"encoding/json"
+	"fmt"
+	"net/http"
+	"time"
+
+	"github.com/gorilla/mux"
+)
+
+// Drink représente une boisson du menu
+type Drink struct {
+	ID        string  `json:"id"`
+	Name      string  `json:"name"`
+	Category  string  `json:"category"` // coffee, tea, cold
+	BasePrice float64 `json:"base_price"`
+}
+
+// OrderStatus représente l'état d'une commande
+type OrderStatus string
+
+const (
+	StatusPending   OrderStatus = "pending"
+	StatusPreparing OrderStatus = "preparing"
+	StatusReady     OrderStatus = "ready"
+	StatusPickedUp  OrderStatus = "picked-up"
+	StatusCancelled OrderStatus = "cancelled"
+)
+
+// Order représente une commande
+type Order struct {
+	ID           string      `json:"id"`
+	DrinkID      string      `json:"drink_id"`
+	DrinkName    string      `json:"drink_name"`
+	Size         string      `json:"size"`   // small, medium, large
+	Extras       []string    `json:"extras"` // milk, sugar, cream, caramel
+	CustomerName string      `json:"customer_name"`
+	Status       OrderStatus `json:"status"`
+	TotalPrice   float64     `json:"total_price"`
+	OrderedAt    time.Time   `json:"ordered_at"`
+}
+
+// Base de données en mémoire
+var drinks = []Drink{
+	{ID: "1", Name: "Espresso", Category: "coffee", BasePrice: 2.0},
+	{ID: "2", Name: "Cappuccino", Category: "coffee", BasePrice: 3.0},
+	{ID: "3", Name: "Latte", Category: "coffee", BasePrice: 3.5},
+	{ID: "4", Name: "Black Tea", Category: "tea", BasePrice: 2.5},
+	{ID: "5", Name: "Green Tea", Category: "tea", BasePrice: 2.5},
+	{ID: "6", Name: "Iced Coffee", Category: "cold", BasePrice: 3.0},
+	{ID: "7", Name: "Iced Tea", Category: "cold", BasePrice: 2.5},
+}
+var orders []Order
+var orderCounter int = 1
+
+func getMenu(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	json.NewEncoder(w).Encode(drinks)
+	fmt.Println("Menu :")
+}
+
+func getDrink(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	vars := mux.Vars(r)
+	drinkID := vars["id"]
+	for _, drink := range drinks {
+		if drink.ID == drinkID {
+			json.NewEncoder(w).Encode(drink)
+			return
+		}
+	}
+}
+
+func calculatePrice(basePrice float64, size string, extras []string) float64 {
+	//prix de base
+	total := basePrice
+
+	//taille
+	switch size {
+	case "small":
+		total *= 0.8
+	case "medium":
+		total *= 1.0 // Prix normal
+	case "large":
+		total *= 1.3
+	}
+
+	//prix des extras
+	total += float64(len(extras)) * 0.50
+
+	//
+	return float64(int(total*100)) / 100
+}
+
+func createOrder(w http.ResponseWriter, r *http.Request) {
+	w.Header().Set("Content-Type", "application/json")
+	var newOrder Order
+	err := json.NewDecoder(r.Body).Decode(&newOrder)
+	//gerer l'erreur
+	if err != nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Invalid request body"})
+		return
+	}
+	//verifie si la boisson existe
+	var foundDrink *Drink
+	for _, drink := range drinks {
+		if drink.ID == newOrder.DrinkID {
+			foundDrink = &drink
+			break
+		}
+	}
+	if foundDrink == nil {
+		w.WriteHeader(http.StatusBadRequest)
+		json.NewEncoder(w).Encode(map[string]string{"error": "Drink not found"})
+		return
+	}
+	// identifiant unique
+	newOrder.ID = fmt.Sprintf("ORD-%03d", orderCounter)
+	orderCounter++
+
+	newOrder.DrinkName = foundDrink.Name
+	newOrder.Status = StatusPending
+	newOrder.OrderedAt = time.Now()
+
+	// prix (a implémenter calculatePrice)
+	newOrder.TotalPrice = calculatePrice(foundDrink.BasePrice, newOrder.Size, newOrder.Extras)
+
+	orders = append(orders, newOrder)
+	w.WriteHeader(http.StatusCreated)
+	json.NewEncoder(w).Encode(newOrder)
+	fmt.Printf("New order created: %+v\n", newOrder)
+
+}
+
+func main() {
+
+	//Routeur mux
+	r := mux.NewRouter()
+
+	//Def les routes
+	r.HandleFunc("/menu", getMenu).Methods("GET")
+	r.HandleFunc("/drinks/{id}", getDrink).Methods("GET")
+	r.HandleFunc("/orders", createOrder).Methods("POST")
+
+	//Démarrer le serveur
+	fmt.Println("Server is starting on port 8080...")
+	if err := http.ListenAndServe(":8080", r); err != nil {
+		fmt.Printf("Error starting server: %s\n", err)
+	}
+}
